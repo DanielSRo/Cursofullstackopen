c576f26c35cb4daa1acaa0da29271a6d6f12087b
Dan SR <119873961+DanielSRo@users.noreply.github.com>
2025-01-19 18:04:13 -0500

    Desarrollo del ejercicio #5 de la parte01

--- part-01/solucion-de-ejercicios/src/App.jsx
+++ part-01/solucion-de-ejercicios/src/App.jsx
@@ -37,30 +37,32 @@ const Total = (props) => {
 
 //Componete principal App:
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   //
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  const totalExercises = course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises
 
 
   return (
     <div>
-      <Header name={course} />
-      <Content content={parts} />
+      <Header name={course.name} />
+      <Content content={course.parts} />
       <Total number={totalExercises} />
     </div>
   )
