60a95cba1004e42fbc604a78d722bc9c911ccbc8
Dan SR <119873961+DanielSRo@users.noreply.github.com>
2025-01-23 16:19:51 -0500

    Desarrollo 'agenda telefonica' del paso 6, de la parte 02

--- part-02/agenda-telefonica/src/App.jsx
+++ part-02/agenda-telefonica/src/App.jsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import Form from './components/Form'
 import PhoneBook from './components/PhoneBook'
 import Find from './components/Find'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFind, setNewFind] = useState('')
 
   const showPerson = persons.filter(name => name.name.toLowerCase() === newFind.toLowerCase())
+
+  /* *** *** *** *** *** *** *** *** *** *** *** *** *** */
+  const request = () => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(request,[])
 
   /* *** *** *** *** *** *** *** *** *** *** *** *** *** */
   const addPerson = (event) => {
